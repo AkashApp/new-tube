5caa9314abba3dba84c775f1762f3c488659062e
akash-borate <akash.borate@parkmate.in>
2025-04-04 22:31:41 +0530

    mux integration

--- src/modules/studio/ui/components/studio-upload-modal.tsx
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -1,27 +1,48 @@
 "use client";
 
+import { ResponsiveModal } from "@/components/responsive-modal";
 import { Button } from "@/components/ui/button";
 import { trpc } from "@/trpc/client";
 import { Loader2Icon, PlusIcon } from "lucide-react";
 import { toast } from "sonner";
-
+import { StudioUploader } from "./studio-uploader";
 
 export const StudioUploadModal = () => {
-    const utils = trpc.useUtils();
-    const create = trpc.videos.craete.useMutation({
-        onSuccess: () => {
-            toast.success("Video created");
-            utils.studio.getMany.invalidate();
-        },
-        onError: (error) => {
-            toast.error(error.message);
-        },
-    });
+  const utils = trpc.useUtils();
+  const create = trpc.videos.craete.useMutation({
+    onSuccess: () => {
+      toast.success("Video created");
+      utils.studio.getMany.invalidate();
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
 
-    return (
-        <Button variant="secondary" onClick={()=>create.mutate()} disabled={create.isPending}>
-            {create.isPending ? <Loader2Icon className="animate-spin"/> : <PlusIcon/>}
-            Create
-        </Button>
-    );
+  return (
+    <>
+    <ResponsiveModal
+     title="Upload Video"
+     open={!!create.data?.url}
+     onOpenChange={()=> create.reset()}
+    >
+        {create.data?.url
+         ? <StudioUploader endpoint={create.data.url} onSuccess={()=>{}}/>
+         : <Loader2Icon/>
+         }
+    </ResponsiveModal>
+      <Button
+        variant="secondary"
+        onClick={() => create.mutate()}
+        disabled={create.isPending}
+      >
+        {create.isPending ? (
+          <Loader2Icon className="animate-spin" />
+        ) : (
+          <PlusIcon />
+        )}
+        Create
+      </Button>
+    </>
+  );
 };
--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -1,5 +1,6 @@
 import { db } from "@/db";
 import { videos } from "@/db/schema";
+import { mux } from "@/lib/mux";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 
 export const videosRouter = createTRPCRouter({
@@ -7,16 +8,28 @@ export const videosRouter = createTRPCRouter({
     craete: protectedProcedure.mutation(async ({ctx})=>{
         const {id: userId} = ctx.user;
 
+        const upload = await mux.video.uploads.create({
+            new_asset_settings:{
+                passthrough: userId,
+                playback_policy: ["public"],
+                // mp4_support: "standard",  // premuim feature
+            },
+            cors_origin: "*", // TODO: In production, set to your url
+        });
+
         const [video] = await db
             .insert(videos)
             .values({
                 userId,
                 title: "Untitled",
+                muxStatus: "waiting",
+                muxUploadId: upload.id,
             })
             .returning();
 
         return {
             video: video,
+            url: upload.url,
         };
     }),
 
